adcb609f237b97f635c7073dfc60c63e82d80ff4
OrrBin <orr.bob@hotmail.com>
2020-04-11 18:04:01 +0300

    GUI microservice introduced

---
+++ thoughts/gui/__main__.py
@@ -0,0 +1,48 @@
+import os
+from os import path
+
+import click
+from thoughts.gui import run_gui_server
+
+
+@click.group()
+def cli():
+    pass
+
+
+def update_api_url(api_url):
+    base_path = path.dirname(__file__)
+    file_path = path.abspath(path.join(base_path,  "static", "env.js"))
+
+    data = ''
+    with open(file_path, 'r') as fin:
+        for line in fin:
+            if 'apiUrl' in line:
+                equals_index = line.index('=')
+                new_line = line[0:equals_index+1]
+                new_line += f" '{api_url}'\n"
+                data += new_line
+            else:
+                data += line
+
+    with open(file_path, 'w') as fout:
+        fout.write(data)
+
+
+@cli.command()
+@click.option('-h', '--host', default='127.0.0.1')
+@click.option('-p', '--port', default='5555')
+def run_server(host, port):
+    # If env variable is defined use it
+    api_url = os.getenv('API_URL')
+    if api_url:
+        update_api_url(api_url)
+
+    try:
+        run_gui_server(host, port)
+    except Exception as error:
+        print(f'ERROR: {error}')
+
+
+if __name__ == '__main__':
+    cli(prog_name='gui')
--- thoughts/gui/gui.py
+++ thoughts/gui/gui.py
@@ -1,11 +1,31 @@
-from flask import Flask, jsonify, send_from_directory
-from flask_cors import CORS
-from thoughts.persistence.databases import init_database
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
-CORS(app)
 
 
-def run_api_server(host, port, database_url):
-    print('starting gui...')
+def run_gui_server(host, port):
+    print('starting api...')
     app.run(host, int(port))
+
+
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+    if path.endswith('.js'):
+        return send_from_directory('./static', path, mimetype='application/javascript')
+    return send_from_directory('./static', path)
+
+
+@app.route('/')
+def root():
+    return send_from_directory('./static', 'index.html')
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying use a webserver process
+    # such as Gunicorn to serve the app.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return 'An internal error occurred [main.py] %s' % e, 500
